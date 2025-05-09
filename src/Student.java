ae2419c4114954edbde4ea1b0255703ccbbeda19
Zabullah <zabee_zh111@yahoo.com>
2025-05-10 01:25:39 +0200

    feat : add student info

---
+++ src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private int id;
+    private  String name;
+    private  String lastName;
+    private  String studentNumber;
+    private  int age;
+    private  double gpa;
+
+
+    // Constructor
+
+    public Student (int id, String name, String lastName, String studentNumber,int age){
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.studentNumber = studentNumber;
+        this.age = age;
+        this.gpa = 0.0;
+    }
+
+}
