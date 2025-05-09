6160dfb83996e748c677d3120c74ca6c121dcbe6
Zabullah <zabee_zh111@yahoo.com>
2025-05-10 01:43:13 +0200

    feat : add getter and setter

--- src/Student.java
+++ src/Student.java
@@ -18,4 +18,38 @@ public class Student {
         this.gpa = 0.0;
     }
 
+    // getters and setters
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        if (gpa >= 0 && gpa <= 20) {
+            this.gpa = gpa;
+        } else {
+            System.out.println("The GPA must be between 0 and 20.");
+        }
+    }
+
 }
